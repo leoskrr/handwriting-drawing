a779f8398c775aa2bf80612fc8b86ebc34a65c99
Leonardo Viana <sonleo2013@gmail.com>
2020-07-26 21:56:24 -0400

    created canvas view

---
+++ handwriting-drawing/CanvasView.swift
@@ -0,0 +1,71 @@
+//
+//  CanvasView.swift
+//  handwriting-drawing
+//
+//  Created by Leonardo Viana on 26/07/20.
+//  Copyright © 2020 Leonardo Viana. All rights reserved.
+//
+
+import UIKit
+
+class CanvasView: UIView {
+    
+    var lineColor: UIColor!
+    var lineWidth: CGFloat!
+    var path:UIBezierPath!
+    var touchPoint: CGPoint!
+    var startingPoint:CGPoint!
+    
+    override func layoutSubviews() {
+        self.clipsToBounds = true
+        self.isMultipleTouchEnabled = false
+        
+        lineColor = UIColor.white
+        lineWidth = 10
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        let touch = touches.first
+        startingPoint = touch?.location(in: self)
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        let touch = touches.first
+        touchPoint = touch?.location(in: self)
+        
+        path = UIBezierPath()
+        path.move(to: startingPoint)
+        path.addLine(to: touchPoint)
+        
+        startingPoint = touchPoint
+        
+        drawShapeLayer()
+    }
+    
+    func drawShapeLayer(){
+        let shapelayer = CAShapeLayer()
+        shapelayer.path = path.cgPath
+        shapelayer.strokeColor = lineColor.cgColor
+        shapelayer.lineWidth = lineWidth
+        shapelayer.fillColor = UIColor.clear.cgColor
+        
+        self.layer.addSublayer(shapelayer)
+        self.setNeedsDisplay()
+    }
+    
+    func clearCanvas(){
+        path.removeAllPoints()
+        
+        self.layer.sublayers = nil
+        self.setNeedsDisplay()
+    }
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
--- handwriting-drawing/ViewController.swift
+++ handwriting-drawing/ViewController.swift
@@ -10,11 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var canvasView: CanvasView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func clearCanvas(_ sender: Any) {
+        canvasView.clearCanvas()
+    }
+    
 }
 
